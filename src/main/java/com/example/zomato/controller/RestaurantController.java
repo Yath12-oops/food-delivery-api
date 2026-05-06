35ae331a74fd75c32008f6919af3e089ac43f917
Yatharth <sachdevayatharth12@gmail.com>
2026-05-06 23:45:39 +0530

    add GET API of list of FoodItems in a restaurant based on veg/Nonveg

--- src/main/java/com/example/zomato/controller/RestaurantController.java
+++ src/main/java/com/example/zomato/controller/RestaurantController.java
@@ -1,15 +1,15 @@
 package com.example.zomato.controller;
 
 import com.example.zomato.dto.request.RestaurantRequest;
+import com.example.zomato.dto.response.FoodItemResponse;
 import com.example.zomato.dto.response.RestaurantResponse;
 import com.example.zomato.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/restaurant")
@@ -22,5 +22,16 @@ public class RestaurantController {
     public ResponseEntity<RestaurantResponse> addRestaurant(@RequestBody RestaurantRequest restaurantRequest){
         RestaurantResponse response=restaurantService.addRestaurant(restaurantRequest);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/option/{option}")
+    public ResponseEntity switchMode(@RequestParam("id") int restaurant_id,@PathVariable("option") String option){
+        try{
+            List<FoodItemResponse> response=restaurantService.switchMode(restaurant_id,option);
+            return new ResponseEntity(response,HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/example/zomato/service/RestaurantService.java
+++ src/main/java/com/example/zomato/service/RestaurantService.java
@@ -1,12 +1,20 @@
 package com.example.zomato.service;
 
+import com.example.zomato.convertors.FoodItemConvertor;
 import com.example.zomato.convertors.RestaurantConvertor;
 import com.example.zomato.dto.request.RestaurantRequest;
+import com.example.zomato.dto.response.FoodItemResponse;
 import com.example.zomato.dto.response.RestaurantResponse;
+import com.example.zomato.exceptions.RestaurantNotFound;
+import com.example.zomato.models.FoodItem;
 import com.example.zomato.models.Restaurant;
 import com.example.zomato.repository.RestaurantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RestaurantService {
@@ -17,5 +25,24 @@ public class RestaurantService {
         Restaurant restaurant= RestaurantConvertor.restaurantRequestToRestaurant(restaurantRequest);
         Restaurant savedRestaurant=restaurantRepo.save(restaurant);
         return RestaurantConvertor.restaurantToRestaurantResponse(savedRestaurant);
+    }
+
+    public List<FoodItemResponse> switchMode(int restaurantId, String option) {
+        Optional<Restaurant> optional=restaurantRepo.findById(restaurantId);
+
+        if(optional.isEmpty()) throw new RestaurantNotFound("Invalid Restaurant id "+ restaurantId);
+
+        Restaurant restaurant=optional.get();
+        List<FoodItem> foodItemList=restaurant.getMenu().getFoodItems();
+
+        boolean isVeg=true;
+        if(option.equals("NonVeg")) isVeg=false;
+
+        List<FoodItemResponse> responseList=new ArrayList<>();
+
+        for(FoodItem foodItem:foodItemList){
+            if(foodItem.isVeg()==isVeg) responseList.add(FoodItemConvertor.foodItemtoFoodItemResponse(foodItem));
+        }
+        return responseList;
     }
 }
